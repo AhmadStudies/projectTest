3f27beadc3f8f91303d06fa23044c22b885c2e62
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2022-07-01 00:39:59 +0200

    create project

---
+++ projectTest/src/main/java/com/massoud/proj/projectTest/App.java
@@ -0,0 +1,13 @@
+package com.massoud.proj.projectTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
