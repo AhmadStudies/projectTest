e092a8ad0d88ccff8be38b14b5560a1211730cf3
Aliherawi <ALiherawi7@gmail.com>
2022-07-05 01:03:01 +0430

    added new field to each class

--- projectTest/src/main/java/com/massoud/proj/projectTest/Teacher.java
+++ projectTest/src/main/java/com/massoud/proj/projectTest/Teacher.java
@@ -1,11 +1,53 @@
 package com.massoud.proj.projectTest;
 
 public class Teacher {
+	private int id;
+	private String name;
+	private String lastName;
+	private String [] courses;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int book = 20;
-		String ty = 30 +"" ;
+	public Teacher(){
+
+	}
+	public Teacher(int id, String name, String lastName, String[] courses){
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.courses = courses
+	}
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+	public String setName(String name){
+		this.name = name;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public String[] getCourses(){
+		return courses;
+	}
+	public void seCourses(String[] courses){
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString(){
+		return 	"name: " + name + 
+				"\nlast name: "+ lastName +
+				"\nid: "+ id;
 	}
 
 }
