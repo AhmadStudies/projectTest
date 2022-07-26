f228a2cd42a82791f7ca330686e8509e217c53c9
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2022-07-26 23:01:51 +0200

    removed the App class and changes in the Teacher class

--- projectTest/src/main/java/com/massoud/proj/projectTest/Teacher.java
+++ projectTest/src/main/java/com/massoud/proj/projectTest/Teacher.java
@@ -3,12 +3,7 @@ package com.massoud.proj.projectTest;
 public class Teacher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int book = 20;
-		String ty = 30 +"" ;
-		
-		int hh = 52;
-		String bb = "sfsdfdf";
+		System.out.println("Some changes happened");
 	}
 
 }
